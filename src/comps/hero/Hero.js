7623feca6254975c7ca0b10c4baf5cac1f16499e
Chris <chris.ware.dev@gmail.com>
2022-05-27 14:19:10 -0700

    Animated Down Arrow

--- src/comps/hero/Hero.js
+++ src/comps/hero/Hero.js
@@ -1,3 +1,4 @@
+import DownArrow from "../../utils/downArrow/DownArrow";
 import styles from "./Hero.module.css";
 
 function Hero() {
@@ -5,11 +6,14 @@ function Hero() {
     <section className={styles.container}>
       <div className={styles.content}>
         <div className={styles.headingContainer}>
-        <div className={styles.line}></div>
-          <h1 className={styles.heading}><span>S</span>trange<span>r</span></h1>
+          <div className={styles.line}></div>
+          <h1 className={styles.heading}>
+            <span>S</span>trange<span>r</span>
+          </h1>
           <h2 className={styles.heading2}>Things</h2>
           <h3 className={styles.heading}>8 ball</h3>
         </div>
+        <DownArrow />
       </div>
     </section>
   );
---
+++ src/utils/downArrow/DownArrow.js
@@ -0,0 +1,11 @@
+import styles from './DownArrow.module.css'
+
+function DownArrow() {
+    return (
+      <div className={styles.container}>
+        <h3 className={styles.arrow}>⌵</h3>
+      </div>
+    );
+}
+
+export default DownArrow
