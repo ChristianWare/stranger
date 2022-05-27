2dc618a5a4d0e99891629941c5dcf47c7b2dfaa5
Chris <chris.ware.dev@gmail.com>
2022-05-27 14:00:59 -0700

    Hero Heading Updates

--- src/comps/hero/Hero.js
+++ src/comps/hero/Hero.js
@@ -7,7 +7,8 @@ function Hero() {
         <div className={styles.headingContainer}>
         <div className={styles.line}></div>
           <h1 className={styles.heading}><span>S</span>trange<span>r</span></h1>
-          <h1 className={styles.heading2}>Things</h1>
+          <h2 className={styles.heading2}>Things</h2>
+          <h3 className={styles.heading}>8 ball</h3>
         </div>
       </div>
     </section>
